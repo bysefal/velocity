0713761ef1b266cdd23ebee4ebf6addf4eb040ab
bysefal <56307858+bysefal@users.noreply.github.com>
2019-11-15 12:45:04 +0100

    correction bug

--- js/signature.js
+++ js/signature.js
@@ -1,11 +1,10 @@
 class Signature {
     constructor(id) {
         this.id = id;
-        this.canvas = document.querySelector(this.id);
+        this.canvas = document.getElementById(this.id);
         this.ctx = this.canvas.getContext('2d');
-
-        this.canvas.width = 300;
-        this.canvas.height = 170;
+        this.canvas.width = this.canvas.clientWidth;
+        this.canvas.height = this.canvas.clientHeight;
         this.rect = this.canvas.getBoundingClientRect();
 
         this.drawing = false;
@@ -14,16 +13,20 @@ class Signature {
         this.canvas.addEventListener('mousedown', this.positionDepart.bind(this));
         this.canvas.addEventListener('mouseup', this.positionFinal.bind(this));
         this.canvas.addEventListener('mousemove', this.dessiner.bind(this));
-    }
+        this.canvas.addEventListener('mouseout', this.positionFinal.bind(this));
+        
+}
 
     positionDepart(e) {
         this.drawing = true;
         this.dessiner(e);
+        this.ctx.beginPath();
     }
 
     positionFinal() {
         this.drawing = false;
-        this.ctx.beginPath();
+        this.prev = undefined;
+        this.ctx.closePath();
     }
 
     dessiner(e) {
@@ -32,15 +35,18 @@ class Signature {
         this.ctx.lineCap = 'round';
 
 		
-
-        this.ctx.lineTo(e.clientX - this.rect.left  + 80, e.clientY - this.rect.top);
-        this.ctx.stroke();
-        this.ctx.beginPath();
-        this.ctx.moveTo(e.clientX - this.rect.left  + 80, e.clientY - this.rect.top);
-        this.ctx.closePath();
+        this.coord = { 'x': e.offsetX, 'y': e.offsetY };
+        if (this.prev !== undefined) {
+            this.ctx.beginPath();
+            this.ctx.moveTo(this.prev.x, this.prev.y);
+            this.ctx.lineTo(this.coord.x, this.coord.y);
+            this.ctx.stroke();
+            this.ctx.closePath();
+            
+        }
+        this.prev = this.coord;
     }
     
     
 }
-
-const sign = new Signature('#sign');
+const sign = new Signature('sign');
